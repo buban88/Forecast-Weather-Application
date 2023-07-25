480481ddfc827b8c9028e4c19814db2808c30911
Subhajit Chatterjee <Subhajit.chatterjee88@gmail.com>
2023-07-25 08:14:27 +0530

    Adding CORS access

--- src/main/java/org/weather/ForecastWeatherApplication/configuration/WeatherConfiguration.java
+++ src/main/java/org/weather/ForecastWeatherApplication/configuration/WeatherConfiguration.java
@@ -3,6 +3,8 @@ package org.weather.ForecastWeatherApplication.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WeatherConfiguration {
@@ -10,5 +12,15 @@ public class WeatherConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
     }
 }
